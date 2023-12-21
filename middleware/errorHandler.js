dfb0f9ddba8c0f55e9baa02196e6d322f549317c
SANTIAGO <90029@sistemas.frc.utn.edu.ar>
2023-12-21 19:15:41 -0300

    ejercicio 5

---
+++ middleware/errorHandler.js
@@ -0,0 +1,14 @@
+// middleware/errorHandler.js
+const errorHandler = (err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const errorResponse = {
+    error: {
+    message: err.message || "Error interno del servidor",
+    code: err.code || "internal_error",
+    },
+    };
+    // Enviar respuesta de error en formato JSON
+    res.status(statusCode).json(errorResponse);
+    };
+    module.exports = errorHandler;
+    
